9a64da7fef6460ac68c1380e6d8b7fb9831d9148
Manuel <manuelt6562@outlook.com>
2022-02-11 12:53:55 -0800

    second commit

--- app.py
+++ app.py
@@ -1,32 +1,46 @@
-"""Blogly application."""
-
-from flask import Flask, request, redirect, render_template
-from models import db, connect_db, User
+from flask import Flask, request, redirect, render_template, flash
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///blogly"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'ihaveasecret'
 
 connect_db(app)
 db.create_all()
 
 
 @app.route('/')
-def home():
-    return redirect('/users')
+def root():
+    """Show recent list of posts, most-recent first."""
 
+    posts = Post.query.order_by(Post.created_at.desc()).limit(5).all()
+    return render_template("posts/homepage.html", posts=posts)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Show 404 NOT FOUND page."""
+
+    return render_template('404.html'), 404
+
+
+##############################################################################
+# User route
 
 @app.route('/users')
-def users():
+def users_index():
+    """Show a page with info on all users"""
+
     users = User.query.order_by(User.last_name, User.first_name).all()
-    return render_template('index.html', users=users)
+    return render_template('users/index.html', users=users)
 
 
-@app.route('/users/new', methods=['GET'])
-def new_user_form():
-    """Form to create new users"""
-    return render_template('new.html')
+@app.route('/users/new', methods=["GET"])
+def users_new_form():
+    """Show a form to create a new user"""
+
+    return render_template('users/new.html')
 
 
 @app.route("/users/new", methods=["POST"])
@@ -40,6 +54,7 @@ def users_new():
 
     db.session.add(new_user)
     db.session.commit()
+    flash(f"User {new_user.full_name} added.")
 
     return redirect("/users")
 
@@ -49,7 +64,7 @@ def users_show(user_id):
     """Show a page with info on a specific user"""
 
     user = User.query.get_or_404(user_id)
-    return render_template('show.html', user=user)
+    return render_template('users/show.html', user=user)
 
 
 @app.route('/users/<int:user_id>/edit')
@@ -57,7 +72,7 @@ def users_edit(user_id):
     """Show a form to edit an existing user"""
 
     user = User.query.get_or_404(user_id)
-    return render_template('edit.html', user=user)
+    return render_template('users/edit.html', user=user)
 
 
 @app.route('/users/<int:user_id>/edit', methods=["POST"])
@@ -71,6 +86,7 @@ def users_update(user_id):
 
     db.session.add(user)
     db.session.commit()
+    flash(f"User {user.full_name} edited.")
 
     return redirect("/users")
 
@@ -82,10 +98,13 @@ def users_destroy(user_id):
     user = User.query.get_or_404(user_id)
     db.session.delete(user)
     db.session.commit()
+    flash(f"User {user.full_name} deleted.")
 
     return redirect("/users")
 
-# POSTS ROUTES
+
+##############################################################################
+# Posts route
 
 
 @app.route('/users/<int:user_id>/posts/new')
--- models.py
+++ models.py
@@ -1,14 +1,12 @@
+"""SQLAlchemy models for blogly."""
+
 import datetime
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
 DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
-
-
-def connect_db(app):
-    db.app = app
-    db.init_app(app)
 
 
 class User(db.Model):
@@ -16,16 +14,19 @@ class User(db.Model):
 
     __tablename__ = "users"
 
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
+
+    posts = db.relationship("Post", backref="user",
+                            cascade="all, delete-orphan")
+
     @property
     def full_name(self):
         """Return full name of user."""
 
         return f"{self.first_name} {self.last_name}"
-
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.Text, nullable=False)
-    last_name = db.Column(db.Text, nullable=False)
-    image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
 
 
 class Post(db.Model):
@@ -41,3 +42,19 @@ class Post(db.Model):
         nullable=False,
         default=datetime.datetime.now)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    @property
+    def friendly_date(self):
+        """Return nicely-formatted date."""
+
+        return self.created_at.strftime("%a %b %-d  %Y, %-I:%M %p")
+
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    db.app = app
+    db.init_app(app)
